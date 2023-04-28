eaf8bb1e8b3ff25c6f0caedc79089e724a3c0c6b
David <shmailov.david@gmail.com>
2023-04-28 21:13:59 +0300

    Final version

--- src/erl_ide_app.erl
+++ src/erl_ide_app.erl
@@ -6,12 +6,14 @@
 -module(erl_ide_app).
 
 -behaviour(application).
--import(ex3_311328322,[main/0]).
+% -import(exm_311328322,[main/0]).
+-import(project_test,[test/0]).
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
     erl_ide_sup:start_link(),
-    ex3_311328322:main(),
+    % exm_311328322:main(),
+    project_test:test(),
     stop(_StartType).
 
 stop(_State) ->
