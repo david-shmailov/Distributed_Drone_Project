d9cbb99224bda2cf4274ec30a1bb3bed36d04ce5
David <shmailov.david@gmail.com>
2023-06-03 22:38:29 +0300

    final version of ex8

--- src/ex_test.erl
+++ src/ex_test.erl
@@ -2,19 +2,34 @@
 -author("David").
 
 -export([test/0]).
--import(ex7_311328322,[steady/1,calc/3]).
+-import(ex8_311328322,[startChat/1, call/1,steadyLink/1, steadyMon/1]).
 
 
+print_per_sec() ->
+    io:format("I am alive~n"),
+    timer:sleep(1000),
+    print_per_sec().
+
+main() ->
+    Pid = spawn(fun () -> print_per_sec() end),
+    link(Pid),
+    io:format("printer Pid: ~p~n", [Pid]),
+    timer:sleep(5000),
+    exit(because).
 
 test() ->
     io:format("**********************************Starting test**********************************~n"),
-    ex7_311328322:steady(fun() -> ex7_311328322:calc(division, 1, 0) end),
-    ex7_311328322:steady(fun() -> ex7_311328322:calc(division, 1, 1) end),
-    ex7_311328322:steady(fun() -> ex7_311328322:calc(division, 1, 2) end),
-    ex7_311328322:steady(fun() -> exit(0) end),
-    ex7_311328322:steady(fun() -> exit(1) end),
-    ex7_311328322:steady(fun() -> throw({mine, "because"}) end),
-    ex7_311328322:steady(fun() -> 1/0 end),
+    % Pid = spawn(fun () -> main() end),
+    % io:format("main Pid: ~p~n", [Pid]),
+    Pid_Local_sender = ex8_311328322:startChat('callee@Legion'),
+    timer:sleep(1000),
+    Pid_Local_sender ! "Hello",
+    % timer:sleep(1000),
+    % ex8_311328322:call("Hey"),
+    % timer:sleep(1000),
+    % Pid_Local_sender ! stats,
+    % timer:sleep(1000),
+    % ex8_311328322:call(stats),
     
     % %% Open the file in write mode
     % {ok, File} = file:open("output.txt", [write]),
